13ef40e49e0b21060d45d6f6df76fec2630172d1
Turner, Zachariah <z00114761@gmail.com>
2026-03-14 13:01:46 -0700

    coding done

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -4,10 +4,40 @@
 #include <sstream> // For parsing lines
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string& fileName);
 
-int main() {
+int main() { 
    parseCSV("students.csv");
 
    return 0;
+}
+void parseCSV(const string& fileName) {
+   ifstream studentsFile(fileName);
+   string line;
+
+   while (getline(studentsFile, line)) {
+      stringstream ss(line);
+      string name;
+      string age;
+      string initial;
+      string isStudent;
+      string grade;
+      
+      
+      getline(ss, name, ' ');
+      getline(ss, age, ' ');
+      getline (ss, initial, ' ');
+      getline (ss, isStudent, ' ');
+      getline (ss, grade, ' ');
+
+      cout<<name<<endl;
+      cout<<age<<endl;
+      cout<<initial<<endl;
+      cout<<isStudent<<endl;
+      cout<<grade<<endl;
+      cout<<endl;
+   }
+   studentsFile.close();
+
+   
 }
